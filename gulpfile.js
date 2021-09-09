0c331223c358a6535d89a35e23180e725f5d6b96
Tristan <tristan@Tristans-iMac.local>
2021-09-09 23:10:27 +0000

    added components

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,14 @@ const sass = require('gulp-sass')(require('sass'));
 
 /* Function to compile the Sass */
 function buildStyles() {
-  return src('index.scss')
+  return src('library/**/*.scss') /* Replace 'index' with '*' -> look for any file with this exstension and compile it */
     .pipe(sass())
     .pipe(dest('css'))
 }
 
 /* Function to watch the Sass to monitor changes and recompile it if needed */
 function watchTask() {
-  watch(['index.scss'], buildStyles)
+  watch(['library/**/*.scss'], buildStyles) /* Replace 'index' with '*' -> same */
 }
 
 exports.default = series(buildStyles, watchTask)
